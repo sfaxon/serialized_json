9280407b5bc872c8f863f36eec0f60b5e8208135
Seth Faxon <seth.faxon@gmail.com>
2015-02-02 14:36:07 -0800

    adding Wine::Varietal class

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -1,2 +1,15 @@
 class Wine < ActiveRecord::Base
+
+  def varietals
+    read_attribute(:varietals).map {|v| Varietal.new(v) }
+  end
+
+  class Varietal
+    attr_accessor :name, :percentage
+
+    def initialize(hash)
+      @name          = hash['name']
+      @percentage    = hash['percentage'].to_i
+    end
+  end
 end
