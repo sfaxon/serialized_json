6702d09590f1bf24ad0a39ba1fd963ad590ee19b
Seth Faxon <seth.faxon@gmail.com>
2015-02-02 14:33:12 -0800

    use structure.sql

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module SerializedJson
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # use structrue.sql because schema.rb does not show love for JSON storage
+    config.active_record.schema_format = :sql
   end
 end
