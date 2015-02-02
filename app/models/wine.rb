104f415f96a175ccfcf05fb27f3e02a9e426d25c
Seth Faxon <seth.faxon@gmail.com>
2015-02-02 15:51:45 -0800

    updating Wine::Varietal and wine edit form

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -4,6 +4,18 @@ class Wine < ActiveRecord::Base
     read_attribute(:varietals).map {|v| Varietal.new(v) }
   end
 
+  # ActionController params will come across as:
+  # {"0"=>{"name"=>"foo", "percentage"=>"10"}, "1"=>{"name"=>"bar", "percentage"=>"90"}}
+  def varietals_attributes=(attributes)
+    varietals = []
+    attributes.each do |index, attrs|
+      next if '1' == attrs.delete("_destroy")
+      attrs[:percentage] = attrs[:percentage].try(:to_i)
+      varietals << attrs
+    end
+    write_attribute(:varietals, varietals)
+  end
+
   class Varietal
     attr_accessor :name, :percentage
 
@@ -11,5 +23,10 @@ class Wine < ActiveRecord::Base
       @name          = hash['name']
       @percentage    = hash['percentage'].to_i
     end
+
+    def persisted?() false; end
+    def new_record?() false; end
+    def marked_for_destruction?() false; end
+    def _destroy() false; end
   end
 end
