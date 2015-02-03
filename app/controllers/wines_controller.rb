6e5f6b0746fc286b487cd7665c949337fe9993bc
Seth Faxon <seth.faxon@gmail.com>
2015-02-02 16:03:01 -0800

    update WineController strong params for varietals

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -69,6 +69,6 @@ class WinesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def wine_params
-      params.require(:wine).permit(:producer, :vintage, :varietals, :designation)
+      params.require(:wine).permit(:producer, :vintage, :designation, varietals_attributes: [:name, :percentage, :_destroy])
     end
 end
